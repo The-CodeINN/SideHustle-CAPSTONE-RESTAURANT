ceefb1854db0d4b48a2578cf40d032e14fc1445c
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-04-27 03:02:36 +0100

    Added the menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitem">MenuItem</div>
 );
 
 export default MenuItem;
