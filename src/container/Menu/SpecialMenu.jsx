63d5f3946e639d8a9053441148a3b6e90da03bd5
Fairbuyng <emijere.richard@gmail.com>
2022-04-27 23:22:43 +0100

    Menu item in progress

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -8,7 +8,7 @@ const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
       <SubHeading title="Menu that fits your pallete" />
-      <h1 className="headtext__colmorant">Today's Special</h1>
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
 
     <div className="app__specialMenu-menu">
@@ -18,7 +18,7 @@ const SpecialMenu = () => (
           {data.wines.map((wine, index) => (
             <MenuItem
               key={wine.title + index}
-              title={wine.price}
+              title={wine.title}
               price={wine.price}
               tags={wine.tags}
             />
@@ -33,7 +33,7 @@ const SpecialMenu = () => (
       </div>
 
       <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className="app__specialMenu-menu_heading">Wine & Beer </p>
+        <p className="app__specialMenu-menu_heading"> Cocktails </p>
         <div className="app__specialMenu-menu_items">
           {data.cocktails.map((cocktail, index) => (
             <MenuItem
