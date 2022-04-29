ce89ccb9ab83ac98afa9e9c440b09728d992c4aa
Fairbuyng <emijere.richard@gmail.com>
2022-04-29 02:16:45 +0100

    Laurels section in progress

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './Laurels.css';
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
+import "./Laurels.css";
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awwards & Recognisition" />
+    </div>
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
