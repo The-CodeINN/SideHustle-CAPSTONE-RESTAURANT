129559d3ef08079a9001bb400e18991bb07b772d
Fairbuyng <emijere.richard@gmail.com>
2022-04-29 02:14:44 +0100

    Chef section done

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="Chef Img" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="Quote" />
+          <p className="p__opensans">We are the best chefs in the world.</p>
+        </div>
+
+        <p className="p__opensans">
+          We make the best cuisines and dishes that are the best in the world with the finest ingredients. We also make the best drinks and
+          cocktails.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="Sign" />
+      </div>
+    </div>
   </div>
 );
 
