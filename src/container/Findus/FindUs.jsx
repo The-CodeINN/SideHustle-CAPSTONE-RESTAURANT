e9487eda60143514110e075ecb68b6fc71bed25d
Fairbuyng <emijere.richard@gmail.com>
2022-05-01 23:57:09 +0100

    Updated

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -11,8 +11,8 @@ const FindUs = () => (
       <div className="app__wrapper-content">
         <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
         <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
-        <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
-        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+        <p className="p__opensans">Mon - Fri: 09:00 am - 10:00 pm</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 09:00 pm</p>
       </div>
       <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Visit Us</button>
     </div>
